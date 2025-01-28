38e2d1d1d7c88089f6f208722be28b98df85728b
root <root@epdj8fdsfge8im43jdqo.auto.internal>
2025-01-28 08:03:20 +0000

    123

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,9 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +88,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -110,3 +110,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+ALLOWED_HOSTS = ['84.201.177.162', '127.0.0.1', 'localhost', 'zaptome2.zapto.org'] 
